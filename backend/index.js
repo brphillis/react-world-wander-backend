a10f89cd4a05727b094f473d881a2cbbc46ff2c2
Brock Phillis <Brockphillis1992@gmail.com>
2022-11-10 09:43:33 +1000

    initial commit react-world-wander-backend

---
+++ backend/index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const app = express();
+const pinRoute = require("./routes/pins");
+const userRoute = require("./routes/users");
+
+dotenv.config();
+
+app.use(express.json());
+
+const cors = require("cors");
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connected!"))
+  .catch((err) => console.log(err));
+
+app.use("/api/pins", pinRoute);
+app.use("/api/users", userRoute);
+
+app.listen(8800, () => {
+  console.log("backend server running");
+});
