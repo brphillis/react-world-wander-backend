c98e5955bfdc53a0f46c5b8417db7ddfacda3592
Brock Phillis <Brockphillis1992@gmail.com>
2022-11-15 13:54:56 +1000

    added JWT authentication

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,6 +1,68 @@
 const User = require("../models/User");
 const router = require("express").Router();
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+//ACCESS & REFRESH TOKENS
+let refreshTokens = [];
+const generateAccessToken = (user) => {
+  return jwt.sign({ id: user._id }, "mySecretKey", {
+    expiresIn: "5s",
+  });
+};
+
+const generateRefreshToken = (user) => {
+  return jwt.sign({ id: user._id }, "myRefreshSecretKey");
+};
+
+router.post("/refresh", (req, res) => {
+  //retrieve refresh token from user
+  const refreshToken = req.body.token;
+
+  //send error if not valid
+  if (!refreshToken) {
+    return res.status(401).json("You are not authenticated");
+  }
+  if (!refreshTokens.includes(refreshToken)) {
+    return res.status(403).json("refresh token is not valid");
+  }
+  jwt.verify(refreshToken, "myRefreshSecretKey", (err, user) => {
+    if (err) {
+      console.log(err);
+    }
+    refreshTokens = refreshTokens.filter((token) => token !== refreshToken);
+
+    const newAccessToken = generateAccessToken(user);
+    const newRefreshToken = generateRefreshToken(user);
+
+    refreshTokens.push(newRefreshToken);
+
+    res.status(200).json({
+      accessToken: newAccessToken,
+      refreshToken: newRefreshToken,
+    });
+  });
+  //if valid, create new access token and send to user
+});
+
+//VERIFY
+const verify = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  console.log("yelp");
+  if (authHeader) {
+    const token = authHeader.split(" ")[1];
+
+    jwt.verify(token, "mySecretKey", (err, user) => {
+      if (err) {
+        return res.status(403).json("Token is not valid!");
+      }
+      req.user = user;
+      next();
+    });
+  } else {
+    res.status(401).json("You are not authenticated!");
+  }
+};
 
 //REGISTER
 router.post("/register", async (req, res) => {
@@ -29,8 +91,14 @@ router.post("/register", async (req, res) => {
 router.post("/login", async (req, res) => {
   try {
     //find user
+    let accessToken = null;
+    let refreshToken = null;
     const user = await User.findOne({ username: req.body.username });
-    if (!user) {
+    if (user) {
+      accessToken = generateAccessToken(user);
+      refreshToken = generateRefreshToken(user);
+      refreshTokens.push(refreshToken);
+    } else {
       res.status(400).json("Wrong username or password");
       return;
     }
@@ -46,9 +114,39 @@ router.post("/login", async (req, res) => {
     }
 
     //send response
-    res.status(200).json({ _id: user._id, username: user.username });
+    res.status(200).json({
+      _id: user._id,
+      username: user.username,
+      accessToken,
+      refreshToken,
+    });
   } catch (err) {
     res.status(500).json(err);
+  }
+});
+
+//LOGOUT
+router.post("/logout", verify, (req, res) => {
+  const refreshToken = req.body.token;
+  refreshTokens = refreshTokens.filter((token) => token !== refreshToken);
+  res.status(200).json("you logged out successfully");
+});
+
+//DELETE
+router.delete("/:id", verify, (req, res) => {
+  console.log(req);
+  if (req.user._id === req.params._id || req.body.username === "adminman") {
+    const { id } = req.body;
+
+    User.findOneAndDelete(id, (err) => {
+      if (err) {
+        return res.send(err);
+      } else {
+        return res.status(200).json({ success: true });
+      }
+    });
+  } else {
+    res.status(403).json("You are not allowed to delete this user!");
   }
 });
 
